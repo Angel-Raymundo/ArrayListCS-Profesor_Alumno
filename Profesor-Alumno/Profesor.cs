d1c21a6dd09d9eba008ed60dea3f1f741be00f85
Angel-Rayo <jimray214@gmail.com>
2025-03-12 17:13:56 -0600

    First commit :)

---
+++ Profesor-Alumno/Profesor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Profesor_Alumno
+{
+    internal class Profesor
+    {
+        private string nombre;
+        private Alumno alumno= new Alumno();
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        internal Alumno Alumno { get => alumno; set => alumno = value; }
+
+        public void capturarDatosAlumno()
+        {
+            Console.WriteLine("Agregue número de lista del alumno:");
+            alumno.Nl = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Agregue nombre del alumno:");
+            alumno.Nombre = Console.ReadLine();
+        }
+
+        public void capturarMaterias(int n)
+        {
+            for (int i = 0; i <= n; i++)
+            {
+                string ma;
+                Console.WriteLine("Ingrese nombre de materia "+(i+1));
+                ma = Console.ReadLine();
+                alumno.agregarMaterias(ma);
+            }
+        }
+
+        public void capturarCalificaciones(int n)
+        {
+            for (int i = 0; i <= n; i++)
+            {
+                int cal;
+                Console.WriteLine("Ingrese calificación de materia " + alumno.Materias[i]);
+                cal = Convert.ToInt32(Console.ReadLine());
+                alumno.agregarCalificaciones(cal);
+            }
+           
+        }
+    }
+}
---
+++ Profesor-Alumno/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data;
+using Profesor_Alumno;
+
+int numMat;
+Dictionary<string, string> diccionario = new Dictionary<string, string>();
+
+Profesor p = new Profesor();
+Console.WriteLine("Ingrese nombre Profesor: ");
+p.Nombre = Console.ReadLine();
+Console.WriteLine("Ingrese los datos del alumno: \n ¿Cuantas materias tiene? ");
+numMat = Convert.ToInt32(Console.ReadLine());
+p.capturarDatosAlumno();
+p.capturarMaterias(numMat);
+p.capturarCalificaciones(numMat);
+
+
+Console.WriteLine("\nMostrando datos...");
+Console.WriteLine("==================");
+Console.WriteLine("Nombre profesor: " + p.Nombre);
+Console.WriteLine("\n===Datos alumno:=== ");
+Console.WriteLine("NL alumno: " + p.Alumno.Nl);
+Console.WriteLine("Nombre alumno: " + p.Alumno.Nombre);
+Console.WriteLine("\n===Materias===");
+
+
+for (int i = 0; i < p.Alumno.Materias.Count; i++)
+{
+    diccionario.Add(p.Alumno.Materias[i].ToString(), p.Alumno.Calificaciones[i].ToString());
+
+}
+
+foreach (var elementos in diccionario)
+    Console.WriteLine(elementos.ToString());
